834afa4f42ad476225e29197cafd8dcf4427cce9
Balajiu97 <yuvabala355@gmail.com>
2025-07-19 20:59:53 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello from Jenkins! Build triggered by commit: $(git rev-parse --short HEAD)"
+echo "Hello world! Build triggered by commit: $(git rev-parse --short HEAD)"
 echo "Current date: $(date)"
 chmod +x hello.sh
 
